c1c91db4ab67a927174e9370ab915b51e9c25e14
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2020-09-18 16:51:33 -0700

    It works better now.

--- examples/src/main.rs
+++ examples/src/main.rs
@@ -8,20 +8,21 @@ struct RefHolder<'a> {
 #[self_referencing]
 pub struct Test<'a, D: 'static + Clone> {
     data: Box<D>,
-    #[tail]
     external: &'a D,
+    #[borrows(data)]
     ptr2: &'this D,
 }
 
 #[self_referencing]
 pub struct Test2 {
     data: Box<i32>,
+    #[borrows(data)]
     ptr: &'this i32,
 }
 
 fn main() {
     let external_int = 123;
-    let test: Test<i32> = Test::new(Box::new(321), |_data| &external_int, |data| data);
+    let test: Test<i32> = Test::new(Box::new(321), &external_int, |data| data);
     let reffed_data = test.use_ptr2(|data| *data);
     println!("{:?}", reffed_data);
     drop(test);
--- ouroboros_macro/src/lib.rs
+++ ouroboros_macro/src/lib.rs
@@ -7,16 +7,36 @@ use syn::parse::{Parse, ParseStream};
 use syn::punctuated::Punctuated;
 use syn::token::Comma;
 use syn::{
-    parenthesized, Expr, Field, Fields, FieldsUnnamed, GenericParam, Generics, Ident, ItemStruct,
-    Lifetime, LifetimeDef, Token, Type, TypeParam, TypeParamBound, Visibility,
+    parenthesized, Attribute, Expr, Field, Fields, FieldsUnnamed, GenericParam, Generics, Ident,
+    ItemStruct, Lifetime, LifetimeDef, Token, Type, TypeParam, TypeParamBound, Visibility,
 };
+
+#[derive(Clone, Copy, PartialEq)]
+enum FieldType {
+    /// Not borrowed by other parts of the struct.
+    Tail,
+    /// Immutably borrowed by at least one other member.
+    Borrowed,
+    /// Mutably borrowed by one other member.
+    BorrowedMut,
+}
+
+impl FieldType {
+    fn is_tail(self) -> bool {
+        self == Self::Tail
+    }
+}
+
+struct BorrowRequest {
+    index: usize,
+    mutable: bool,
+}
 
 struct StructFieldInfo {
     name: Ident,
     typ: Type,
-    /// True if no other fields in the struct can refer to this field. This allows its type to be
-    /// more flexible.
-    is_tail: bool,
+    field_type: FieldType,
+    borrows: Vec<BorrowRequest>,
 }
 
 fn replace_this_with_static(input: TokenStream2) -> TokenStream2 {
@@ -39,18 +59,104 @@ fn replace_this_with_static(input: TokenStream2) -> TokenStream2 {
         .collect()
 }
 
+fn handle_borrows_attr(
+    field_info: &mut [StructFieldInfo],
+    attr: &Attribute,
+    borrows: &mut Vec<BorrowRequest>,
+) {
+    let mut borrow_mut = false;
+    let mut waiting_for_comma = false;
+    let tokens = attr.tokens.clone();
+    let tokens = if let Some(TokenTree::Group(group)) = tokens.into_iter().next() {
+        group.stream()
+    } else {
+        panic!("Invalid syntax for borrows() macro.");
+    };
+    for token in tokens {
+        if let TokenTree::Ident(ident) = token {
+            if waiting_for_comma {
+                panic!("Unexpected '{}', expected comma.", ident);
+            }
+            let istr = ident.to_string();
+            if istr == "mut" {
+                if borrow_mut {
+                    panic!("Unexpected double 'mut' in borrows() macro.");
+                }
+                borrow_mut = true;
+            } else {
+                let index = field_info.iter().position(|item| item.name == istr);
+                let index = if let Some(v) = index {
+                    v
+                } else {
+                    panic!(
+                        concat!(
+                            "Unknown identifier '{}', make sure that it is spelled ",
+                            "correctly and defined above the location it is borrowed."
+                        ),
+                        istr
+                    );
+                };
+                if borrow_mut {
+                    if field_info[index].field_type == FieldType::Borrowed {
+                        panic!(
+                            "Cannot borrow '{}' as mut as it was previously borrowed immutably.",
+                            istr,
+                        );
+                    }
+                    if field_info[index].field_type == FieldType::BorrowedMut {
+                        panic!("Cannot borrow '{}' mutably more than once.", istr,)
+                    }
+                    field_info[index].field_type = FieldType::BorrowedMut;
+                } else {
+                    if field_info[index].field_type == FieldType::BorrowedMut {
+                        panic!(
+                            "Cannot borrow '{}' again as it was previously borrowed mutably.",
+                            istr,
+                        );
+                    }
+                    field_info[index].field_type = FieldType::Borrowed;
+                }
+                borrows.push(BorrowRequest {
+                    index,
+                    mutable: borrow_mut,
+                });
+                waiting_for_comma = true;
+                borrow_mut = false;
+            }
+        } else if let TokenTree::Punct(punct) = token {
+            if punct.as_char() == ',' {
+                if waiting_for_comma {
+                    waiting_for_comma = false;
+                } else {
+                    panic!("Unexpected extra comma in borrows() macro.");
+                }
+            } else {
+                panic!(
+                    "Unexpected punctuation {}, expected comma or identifier.",
+                    punct
+                );
+            }
+        } else {
+            panic!("Unexpected token {}, expected comma or identifier.", token);
+        }
+    }
+}
+
 #[proc_macro_attribute]
 pub fn self_referencing(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let original_struct_def: ItemStruct = syn::parse_macro_input!(item);
+    let struct_name = &original_struct_def.ident;
+    let mod_name = format_ident!("ouroboros_impl_{}", struct_name.to_string().to_snake_case());
+    let visibility = &original_struct_def.vis;
 
     // Actual data struct generation & metadata gathering.
     let mut actual_struct_def = original_struct_def.clone();
-    actual_struct_def.vis = Visibility::Inherited;
+    actual_struct_def.vis = syn::parse_quote! { pub };
     let mut field_info = Vec::new();
     match &mut actual_struct_def.fields {
         Fields::Named(fields) => {
             for field in &mut fields.named {
-                let mut is_tail = false;
+                let mut borrows = Vec::new();
                 for (index, attr) in field.attrs.iter().enumerate() {
                     let path = &attr.path;
                     if path.leading_colon.is_some() {
@@ -59,8 +165,8 @@ pub fn self_referencing(_attr: TokenStream, item: TokenStream) -> TokenStream {
                     if path.segments.len() != 1 {
                         continue;
                     }
-                    if path.segments.first().unwrap().ident.to_string() == "tail" {
-                        is_tail = true;
+                    if path.segments.first().unwrap().ident.to_string() == "borrows" {
+                        handle_borrows_attr(&mut field_info[..], attr, &mut borrows);
                         field.attrs.remove(index);
                         break;
                     }
@@ -68,7 +174,8 @@ pub fn self_referencing(_attr: TokenStream, item: TokenStream) -> TokenStream {
                 field_info.push(StructFieldInfo {
                     name: field.ident.clone().expect("Named field has no name."),
                     typ: field.ty.clone(),
-                    is_tail,
+                    field_type: FieldType::Tail,
+                    borrows,
                 });
             }
         }
@@ -78,18 +185,21 @@ pub fn self_referencing(_attr: TokenStream, item: TokenStream) -> TokenStream {
     if field_info.len() < 2 {
         panic!("Self-referencing structs must have at least 2 members.");
     }
-    // Last element is always a tail, by definition. Nothing can possibly refer to it.
-    let last_index = field_info.len() - 1;
-    field_info[last_index].is_tail = true;
     let mut has_non_tail = false;
     for field in &field_info {
-        if !field.is_tail {
+        if !field.field_type.is_tail() {
             has_non_tail = true;
             break;
         }
     }
     if !has_non_tail {
-        panic!("Self-referencing struct cannot be made entirely of tail fields.");
+        panic!(
+            concat!(
+                "Self-referencing struct cannot be made entirely of tail fields, try adding ",
+                "#[borrows({0})] to a member defined after {0}."
+            ),
+            field_info[0].name
+        );
     }
     // Reverse the order of all members. We ensure that items in the struct are only dependent
     // on references to items above them. Rust drops items in a struct in forward declaration order.
@@ -118,7 +228,7 @@ pub fn self_referencing(_attr: TokenStream, item: TokenStream) -> TokenStream {
                 GenericParam::Lifetime(lt) => {
                     let lifetime = &lt.lifetime;
                     arguments.push(quote! { #lifetime });
-                },
+                }
                 GenericParam::Const(_) => unimplemented!(),
             }
         }
@@ -126,32 +236,49 @@ pub fn self_referencing(_attr: TokenStream, item: TokenStream) -> TokenStream {
     };
 
     // Constructor generation.
-    let struct_name = &original_struct_def.ident;
     let constructor_def: TokenStream2 = {
         let mut params: Vec<TokenStream2> = Vec::new();
         let mut code: Vec<TokenStream2> = Vec::new();
-        let mut field_builder_params: Vec<TokenStream2> = Vec::new();
-        let mut field_builder_args: Vec<TokenStream2> = Vec::new();
         for field in &field_info {
             let field_name = &field.name;
             let field_type = &field.typ;
-            // If we haven't gotten past fields that can be referenced yet, then we can just accept
-            // the value of this field as a straight argument with no builder necessary.
-            if field_builder_params.len() == 0 {
-                params.push(quote!( #field_name : #field_type ));
-            } else {
+
+            if field.borrows.len() > 0 {
+                let mut field_builder_params = Vec::new();
+                let mut field_builder_args = Vec::new();
+                for borrow in &field.borrows {
+                    if borrow.mutable {
+                        unimplemented!();
+                    } else {
+                        let field = &field_info[borrow.index];
+                        let field_type = &field.typ;
+                        field_builder_params.push(quote! {
+                            &'this <#field_type as ::std::ops::Deref>::Target
+                        });
+                        let ref_name = format_ident!("{}_illegal_static_reference", field.name);
+                        field_builder_args.push(ref_name);
+                    }
+                }
                 let builder_name = format_ident!("{}_builder", field_name);
-                params.push(quote! { #builder_name : impl for<'this> FnOnce(#(#field_builder_params),*) -> #field_type });
+                params.push(quote! {
+                    #builder_name : impl for<'this> FnOnce(#(#field_builder_params),*) -> #field_type
+                });
                 code.push(quote! { let #field_name = #builder_name (#(#field_builder_args),*); })
+            } else {
+                // If it doesn't need to borrow anything, we can just copy it straight in to the
+                // struct without any fancy builder nonsense.
+                params.push(quote!( #field_name : #field_type ));
             }
-            if !field.is_tail {
+
+            if field.field_type == FieldType::Borrowed {
                 let ref_name = format_ident!("{}_illegal_static_reference", field_name);
                 code.push(quote! {
-                    let #ref_name = unsafe { ::ouroboros::macro_help::stable_deref_and_strip_lifetime(&#field_name) };
+                    let #ref_name = unsafe {
+                        ::ouroboros::macro_help::stable_deref_and_strip_lifetime(&#field_name)
+                    };
                 });
-                field_builder_params
-                    .push(quote! { &'this <#field_type as ::std::ops::Deref>::Target });
-                field_builder_args.push(quote! { #ref_name });
+            } else if field.field_type == FieldType::BorrowedMut {
+                unimplemented!();
             }
         }
         let mut field_names = Vec::new();
@@ -174,7 +301,7 @@ pub fn self_referencing(_attr: TokenStream, item: TokenStream) -> TokenStream {
         let user_name = format_ident!("use_{}", &field.name);
         // If the field is not a tail, we need to serve up the same kind of reference that other
         // members in the struct may have borrowed to ensure safety.
-        if field.is_tail {
+        if field.field_type == FieldType::Tail {
             users.push(quote! {
                 pub fn #user_name <'outer_borrow, ReturnType>(
                     &'outer_borrow self,
@@ -183,7 +310,7 @@ pub fn self_referencing(_attr: TokenStream, item: TokenStream) -> TokenStream {
                     user(&self. #field_name)
                 }
             });
-        } else {
+        } else if field.field_type == FieldType::Borrowed {
             users.push(quote! {
                 pub fn #user_name <'outer_borrow, ReturnType>(
                     &'outer_borrow self,
@@ -192,6 +319,8 @@ pub fn self_referencing(_attr: TokenStream, item: TokenStream) -> TokenStream {
                     user(&*self. #field_name)
                 }
             });
+        } else if field.field_type == FieldType::BorrowedMut {
+            unimplemented!()
         }
     }
 
@@ -203,12 +332,14 @@ pub fn self_referencing(_attr: TokenStream, item: TokenStream) -> TokenStream {
         for field in field_info.iter().rev() {
             let field_name = &field.name;
             let field_type = &field.typ;
-            if field.is_tail {
+            if field.field_type == FieldType::Tail {
                 fields.push(quote! { pub #field_name: &'outer_borrow #field_type });
                 field_assignments.push(quote! { #field_name: &self.#field_name });
-            } else {
+            } else if field.field_type == FieldType::Borrowed {
                 fields.push(quote! { pub #field_name: &'outer_borrow <#field_type as ::std::ops::Deref>::Target });
                 field_assignments.push(quote! { #field_name: &*self.#field_name });
+            } else if field.field_type == FieldType::BorrowedMut {
+                unimplemented!()
             }
         }
         let all_fields_struct = if generic_producers.params.len() == 0 {
@@ -249,13 +380,16 @@ pub fn self_referencing(_attr: TokenStream, item: TokenStream) -> TokenStream {
     };
 
     let final_data = TokenStream::from(quote! {
-        #actual_struct_def
-        pub #all_fields_struct
-        impl #generic_producers #struct_name <#(#generic_consumers),*> {
-            #constructor_def
-            #(#users)*
-            #use_all_fields_fn
+        mod #mod_name {
+            #actual_struct_def
+            pub #all_fields_struct
+            impl #generic_producers #struct_name <#(#generic_consumers),*> {
+                #constructor_def
+                #(#users)*
+                #use_all_fields_fn
+            }
         }
+        #visibility use #mod_name :: #struct_name;
     });
     // eprintln!("{:#?}", final_data);
     final_data
